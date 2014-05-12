5a4033e60b119e41e3fa8b0470b862b86212f4b9
hemal patel <Hml@hemals-Mac-mini.local>
2014-05-12 23:22:32 +0530

    Initial Commit

---
+++ Polygon/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Polygon
+//
+//  Created by hemal patel on 12/05/14.
+//  Copyright (c) 2014 Hml. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Polygon/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Polygon
+//
+//  Created by hemal patel on 12/05/14.
+//  Copyright (c) 2014 Hml. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
